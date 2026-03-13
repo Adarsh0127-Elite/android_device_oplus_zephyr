cdf74821b6548b4cd66e848967827fe380620133
Thundergod_thor <adityasingh6901012a@gmail.com>
2026-03-13 10:44:50 +0000

    zephyr: Udfps experiments

--- device.mk
+++ device.mk
@@ -125,8 +125,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
-$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
-$(call soong_config_set,surfaceflinger,mtk_dim_layer,OnScreenFingerprintPressedIcon)
+$(call soong_config_set,surfaceflinger,has_mtk_udfps,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
