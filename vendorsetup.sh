0979804c53cd3fc99a9c5727ee8457173aef4178
Λᴅᴀʀꜱʜ ⚡ <adarshmoharana027@gmail.com>
2026-03-27 22:33:45 +0530

    zephyr: Include Gpu fix patch
    Signed-off-by: Λᴅᴀʀꜱʜ ⚡ <adarshmoharana027@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Applying frameworks/native patches"
+cd frameworks/native
+echo "Applying Add MTK GED KPI support to fix broken Mediatek gpufreq"
+git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git
+git cherry-pick 0f64f8863865458374c1039426fd056a966e7c48
+cd ../..
+
 echo "Clonning IMS"
 git clone -b android-16-qpr2 https://github.com/Adarsh0127-Elite/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
