c5c7f96905133c01a918badefc301fab47194dec
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-07-08 07:21:05 +0000

    zephyr: Enable MTK GHBM udfps impl and define the DimLayer name
    Required with latest changes done on frameworks/base & frameworks/native
    Changes:
    [1]https://github.com/Adarsh0127-Elite/android_frameworks_native/commit/e4faa3cb29d86e5a922a0d9136d71b5a6c339cae
    [2]https://github.com/Adarsh0127-Elite/android_frameworks_base/commit/4dde9824672f84e59249fa5b183e483543d01881
    
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Fingerprint
+$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set,surfaceflinger,mtk_dim_layer,OnScreenFingerprintPressedIcon)
+
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
 
